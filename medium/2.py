31826a2bc8283c752fe77caa1e8ccaf8ba8fd86e
kevma271 <kevma271@student.liu.se>
2026-09-10 23:28:53 +0900

    problem 2: solved it cleaneeer

--- medium/2.py
+++ medium/2.py
@@ -5,50 +5,23 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        current_l1: ListNode = l1
-        current_l2: ListNode = l2
-        tens_sum: int = 0
+        carry: int = 0
         dummy: ListNode = ListNode()
         current: ListNode = dummy
 
-        while current_l1 is not None and current_l2 is not None:
-            current.next = ListNode()
+        while l1 is not None or l2 is not None or carry != 0:
+            val1 = l1.val if l1 is not None else 0
+            val2 = l2.val if l2 is not None else 0
+
+            new_sum = val1+val2+carry
+            digit = new_sum % 10
+            carry = new_sum // 10
+
+            current.next = ListNode(digit)
             current = current.next
-            node_sum: int = current_l1.val + current_l2.val
-            node_sum+=tens_sum
-            
-            digit: int = node_sum % 10
-            tens_sum: int = node_sum // 10
 
-            current.val = digit
-
-            current_l1 = current_l1.next
-            current_l2 = current_l2.next
-
-        if current_l1 is None and current_l2 is not None:
-            while current_l2 is not None:
-                current.next = ListNode()
-                current = current.next
-                node_sum: int = current_l2.val + tens_sum
-                digit: int = node_sum % 10
-                tens_sum: int = node_sum // 10
-
-                current.val = digit
-                current_l2 = current_l2.next
-
-        if current_l1 is not None and current_l2 is None:
-            while current_l1 is not None:
-                current.next = ListNode()
-                current = current.next
-                node_sum: int = current_l1.val + tens_sum
-                digit: int = node_sum % 10
-                tens_sum: int = node_sum // 10
-
-                current.val = digit
-                current_l1 = current_l1.next
-
-        if tens_sum != 0:
-            current.next = ListNode(tens_sum)
+            l1 = l1.next if l1 is not None else None
+            l2 = l2.next if l2 is not None else None
 
         return dummy.next
 
